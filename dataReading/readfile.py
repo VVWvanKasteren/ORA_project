108e483036853c9451a1ef7b4624c352a1a33306
VVWvanKasteren <145544611+VVWvanKasteren@users.noreply.github.com>
2024-05-28 19:17:22 +0200

    Add files via upload

--- dataReading/readfile.py
+++ dataReading/readfile.py
@@ -5,18 +5,19 @@ Created on Tue Apr 23 22:55:13 2024
 @author: S_Schreuder
 """
 
-import numpy as np 
+import numpy as np
 import ast
 import pandas as pd
+import math
 
-with open("instances/sprint_hint03.dat", 'r', errors = 'ignore') as file:
+with open("sprint_hint03.dat", 'r', errors = 'ignore') as file:
     lines = [line.strip().split() for line in file]
     n_days = 28
     shift_types = []
     for shift in lines[4]:
         if shift != " ":
             shift_types.append(shift)
-    
+
     n_shift_types = len(shift_types)
     n_contracts = int(lines[n_shift_types + 13][-1][-1]) + 1
     nurse_line = n_shift_types + n_contracts + 21
@@ -30,7 +31,7 @@ with open("instances/sprint_hint03.dat", 'r', errors = 'ignore') as file:
     for k in range(n_nurses):
         idx = nurse_line + 4*(k+1)
         ns = int(lines[idx][1][4:-1])
-        cs = lines[idx + 1]   
+        cs = lines[idx + 1]
         for i, shift_type in enumerate(shift_types):
             if shift_type in cs:
                 comp_shifts[ns][i] = 1
@@ -42,26 +43,26 @@ with open("instances/sprint_hint03.dat", 'r', errors = 'ignore') as file:
     while lines[curr_index][0][0] == '[':
         req = lines[curr_index]
         req_rel = ast.literal_eval(req[0].replace('[', "['").replace(',', "','").replace(']', "']"))
-        shift_off_reqs 
+        shift_off_reqs
         for i, shift_type in enumerate(shift_types):
             if shift_type in req_rel[1]: #
                 shift_off_reqs[int(req_rel[0][1:])][int(req_rel[2])-1][i] = int(req[1])
         curr_index += 1
-    
+
     curr_index += 9
     wknds_by_c = np.zeros((n_contracts, n_days))
     while lines[curr_index]:
         for i in lines[curr_index + 1]:
-            wknds_by_c[int(lines[curr_index][1][4:-1])][int(i)-1] = 1 
+            wknds_by_c[int(lines[curr_index][1][4:-1])][int(i)-1] = 1
         curr_index += 3
-     
+
     while 1:
         if lines[curr_index]:
             if lines[curr_index][0] == 'param':
                 if lines[curr_index][1] == 'r':
                     break
         curr_index += 1
-        
+
     demand = np.zeros((n_days, n_shift_types))
     st2 = lines[curr_index+1][:n_shift_types]
     for day in range(28):
@@ -72,12 +73,12 @@ with open("instances/sprint_hint03.dat", 'r', errors = 'ignore') as file:
             demand[day][st] = int(reorval[st])
     demand = pd.DataFrame(demand)
     demand.columns = shift_types
-            
+
     curr_index += 34
     nurse_contracts = np.zeros((n_nurses))
     for nrs in range(n_nurses):
         nurse_contracts[int(lines[curr_index+nrs][0][1:])] = int(lines[curr_index+nrs][1][-1])
-    
+
     curr_index += n_nurses + 2
     param_names = []
     contr_param = np.empty((14, n_contracts),dtype=object)
@@ -90,35 +91,35 @@ with open("instances/sprint_hint03.dat", 'r', errors = 'ignore') as file:
 
     n_unw_pat = np.zeros((n_contracts))
     len_unw_pat = [[] for _ in range(n_contracts)]
-    if lines[curr_index + 26*(n_contracts + 4) + 1 + c][0] != ';': 
+    if lines[curr_index + 26*(n_contracts + 4) + 1 + c][0] != ';':
         for c in range(n_contracts):
             n_unw_pat[c] = int(lines[curr_index + 26*(n_contracts + 4) + 1 + c][1])
-    curr_index = curr_index + 27*(n_contracts + 4) 
-    
+    curr_index = curr_index + 27*(n_contracts + 4)
+
     for i in range(n_contracts):
         for j in range(int(n_unw_pat[i])):
             len_unw_pat[i].append(int(lines[curr_index][2]))
             curr_index += 1
 
     curr_index += 3
-    unw_pats = [[] for _ in range(n_contracts)]      
+    unw_pats = [[] for _ in range(n_contracts)]
     for i in range(n_contracts):
-        unw_pats[i] = [[] for _ in range(len(len_unw_pat[i]))]  
+        unw_pats[i] = [[] for _ in range(len(len_unw_pat[i]))]
         for j in range(len(len_unw_pat[i])):
             pat_len = len_unw_pat[i][j]
             for k in range(pat_len):
                 unw_pats[i][j].append(lines[curr_index][3])
                 curr_index += 1
-                
+
     curr_index += 3
-    w_unw_pats = [[] for _ in range(n_contracts)] 
+    w_unw_pats = [[] for _ in range(n_contracts)]
     for i in range(n_contracts):
         for j in range(len(len_unw_pat[i])):
             w_unw_pats[i].append(int(lines[curr_index][2]))
             curr_index += 1
 
     curr_index += 3
-    if lines[curr_index][0] == 'C0': 
+    if lines[curr_index][0] == 'C0':
         for k in range(n_contracts):
             contr_param[12][k] = [np.sign(contr_param[12][k][1]), contr_param[12][k][1]]
             contr_param[13][k] = [1, int(lines[-4][2])]
@@ -134,7 +135,7 @@ for a in range(n_contracts):
     lday = 0
     for day in range(n_days):
         if wknds_by_c[a][day] == 1:
-            lday, fday = day, day     
+            lday, fday = day, day
             while wknds_by_c[a][lday] == 1:
                 lday += 1
             while wknds_by_c[a][fday] == 1:
@@ -151,7 +152,98 @@ print('\nshift-off request ([nurse][day][shift type] = weight)\n', shift_off_req
 print('\nWeekend days by contract type:\n', weekends_contract)
 print('\ndemand\n', demand)
 print('\nnurse contract types:', nurse_contracts)
-print('\nParameters by contract (value, weight):') 
+print('\nParameters by contract (value, weight):')
 print(contr_param)
 print('\nUnwanted patterns by contract:', unw_pats)
 print('\nWeights unwanted patterns by contract:', w_unw_pats)
+
+def createPar(shift_types, n_contracts, n_nurses, comp_shifts, shift_off_reqs, weekends_contract, demand, nurse_contracts, contr_param, unw_pats, w_unw_pats, n_days):
+
+    print("")
+    print("PARAMETERS\n")
+    # Paramater creation in order of the article
+    # A "#" means the parameter was already clearly identified by just reading in the data
+
+    # The N parameter
+    N = np.arange(1,n_nurses+1)
+    print(f"N: {N}\n")
+
+    # S parameter, clearly read in
+    #shift_types
+    print(f"S: {shift_types}\n")
+
+    # S subsets
+    S_a = [shift_types[0]]
+    S_b = [i for i in shift_types if i != 'N']
+    print(f"S subset(night): {S_a}")
+    print(f"S subset(others): {S_b}\n")
+
+    # D parameter
+    D = np.arange(1, n_days+1)
+    print(f"D: {D}\n")
+
+    # r_ds parameter
+    #demand
+    print("r_ds:")
+    print(demand)
+    print("")
+
+    # Pie parameter
+    Pi = []
+    for i in range(1, n_days+1):
+        for j in range(i, n_days+1):
+            Pi.append([i,j])
+    print(f"Pie: {Pi}\n")
+
+    # W_n parameter
+    W = {}
+    for i in range(n_nurses):
+        W[i+1] = []
+        w_count = 1
+        count = 0
+        if nurse_contracts[i] == 0:
+            for j in range(len(weekends_contract[0])):
+                if weekends_contract[0][j][0] == 1:
+                    count += 1
+                    if count % 2 == 1:
+                        W[i+1].append(w_count)
+                        w_count +=1
+        if nurse_contracts[i] == 1:
+            for j in range(len(weekends_contract[1])):
+                if weekends_contract[1][j][0] == 1:
+                    count += 1
+                    if count % 2 == 1:
+                        W[i+1].append(w_count)
+                        w_count +=1
+        if nurse_contracts[i] == 2:
+            for j in range(len(weekends_contract[2])):
+                if weekends_contract[2][j][0] == 1:
+                    count += 1
+                    if count % 2 == 1:
+                        W[i+1].append(w_count)
+                        w_count +=1
+    print(f"W_n: {W}\n")
+
+    # D_in parameter
+    temp = {}
+    count = 0
+    for i in range(1, n_contracts+1):
+        temp[i] = []
+        for j in range(len(weekends_contract[i-1])):
+            if weekends_contract[i-1][j][0] == 1:
+                count+=1
+                if count % 2 == 1:
+                    temp[i].append([j,j+1])
+
+    D = {}
+    for i in range(n_nurses):
+        if nurse_contracts[i] == 0:
+            D[i+1] = temp[1]
+        elif nurse_contracts[i] == 1:
+            D[i+1] = temp[2]
+        else:
+            D[i+1] = temp[3]
+    print(f"D_in: {D}")
+
+
+createPar(shift_types, n_contracts, n_nurses, comp_shifts, shift_off_reqs, weekends_contract, demand, nurse_contracts, contr_param, unw_pats, w_unw_pats, n_days)
